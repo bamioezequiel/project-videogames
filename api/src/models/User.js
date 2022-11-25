ab535a2b20d0da5af7bb5129d2437074dfa63cfb
Bamio Ezequiel <ezequielbamiok@gmail.com>
2022-11-25 10:59:26 -0300

    fixed

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../db.js";
+import { sequelize } from "./../../db.js";
 import crypto from "crypto";
 
 export const User = sequelize.define(
