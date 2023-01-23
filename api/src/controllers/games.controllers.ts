1c217b9a354b66b6f466528b9138d35a65bc7463
Bamio Ezequiel <ezequielbamiok@gmail.com>
2023-01-23 14:05:02 -0300

    added games services

--- api/src/controllers/games.controllers.ts
+++ api/src/controllers/games.controllers.ts
@@ -1,26 +1,15 @@
 import { Request, Response } from "express";
 import GameModel from "../models/game.model";
+import { createGame, deletePermanentlyGame, getGames, patchGame, updateGame, updateStatusGame } from "../services/game.service";
 import { dataGames } from "./../data/games";
 
 export const getAllGames = async (req: Request, res: Response) => {
   const { active } = req.query;
 
   try {
-    if (active === "true") {
-      const games = await GameModel.find({
-        where: {
-          active: true,
-        },
-      });
+    const allGames = await getGames(`${active}`, '');
 
-      res.send(games);
-    } else {
-      const allGames = await GameModel.find({
-        paranoid: false,
-      });
-
-      res.send(allGames);
-    }
+    res.send(allGames);
   } catch (error) {
     res.status(404).send(error);
   }
@@ -29,9 +18,7 @@ export const getAllGames = async (req: Request, res: Response) => {
 export const getGamesById = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const game = await GameModel.findById(id);
-    //paranoid: false
-    if(!game) return res.status(400).send('The game not found')
+    const game = await getGames('true', id)
     res.send(game);
   } catch (error) {
     res.status(404).send(error);
@@ -42,11 +29,8 @@ export const statusGame = async (req: Request, res: Response) => {
   const { id } = req.params;
   let { status }: any = req.query;
   if (status === undefined) status = false;
-  console.log(status);
   try {
-    const disabledGame = await GameModel.findByIdAndUpdate(id, {
-      active: status,
-    });
+    const disabledGame = await updateStatusGame(id, status);
 
     res.send(`The game was ${status ? "active" : "disabled"}`);
   } catch (error) {
@@ -57,7 +41,7 @@ export const statusGame = async (req: Request, res: Response) => {
 export const deleteGame = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
-    const deletedGame = await GameModel.findByIdAndDelete(id);
+    const deletedGame = await deletePermanentlyGame(id);
     res.send("The game was permanently removed");
   } catch (error) {
     res.status(404).send(error);
@@ -66,14 +50,10 @@ export const deleteGame = async (req: Request, res: Response) => {
 
 export const featuredGame = async (req: Request, res: Response) => {
   const { id } = req.params;
-  let { featured } = req.query;
-  if (featured === undefined) featured = "false";
-  try {
-    const game = await GameModel.findByIdAndUpdate(id, {
-      featured,
-    });
-    if (!game) res.status(404).send("The game is null");
-    res.send(await GameModel.findById(`${game?._id}`));
+  let { featured }: any = req.query;
+  if (featured === undefined) featured = false;
+  try {   
+    res.send(await patchGame(id, 'featured', featured));
   } catch (error) {
     res.status(404).send(error);
   }
@@ -81,15 +61,10 @@ export const featuredGame = async (req: Request, res: Response) => {
 
 export const isNewGame = async (req: Request, res: Response) => {
   const { id } = req.params;
-  let { isNew } = req.query;
-  if (isNew === undefined) isNew = "false";
+  let { isNew }: any = req.query;
+  if (isNew === undefined) isNew = false;
   try {
-    const game = await GameModel.findByIdAndUpdate(id, {
-      is_new: isNew,
-    });
-    if (!game) res.status(404).send("The game is null");
-    res.send(await GameModel.findById(`${game?._id}`));
-    // paranoid: false
+    res.send(await patchGame(id, 'is_new', isNew));
   } catch (error) {
     res.status(404).send(error);
   }
@@ -98,26 +73,8 @@ export const isNewGame = async (req: Request, res: Response) => {
 export const postGame = async (req: Request, res: Response) => {
   const bodyGame = req.body;
   try {
-    const game = await GameModel.findOne({ name: bodyGame.name });
-    if (game) return res.status(400).send(`This game already exists`);
-    const createdGame = await GameModel.create({
-      name: bodyGame.name,
-      description: bodyGame.description,
-      released: bodyGame.released,
-      main_image: bodyGame.main_image,
-      short_screenshots: bodyGame.short_screenshots,
-      rating: bodyGame.rating,
-      price: Number(bodyGame.price),
-      on_sale: Number(bodyGame.on_sale),
-      stock: Number(bodyGame.stock),
-      featured: bodyGame.featured ? true : false,
-      is_new: bodyGame.is_new ? true : false,
-      platforms: bodyGame.platforms,
-      genres: bodyGame.genres,
-      tags: bodyGame.tags,
-    });
+    res.status(201).send(await createGame(bodyGame));
 
-    res.status(201).send(`The game was created successfully!`);
   } catch (error) {
     res.status(404).send(error);
   }
@@ -126,17 +83,7 @@ export const postGame = async (req: Request, res: Response) => {
 export const putGame = async (req: Request, res: Response) => {
   const bodyGame = req.body;
   try {
-    const updatedGame = await GameModel.findByIdAndUpdate(
-      bodyGame._id,
-      bodyGame
-    );
-
-    if (!updatedGame) {
-      return res
-        .status(400)
-        .send(`The game could not be modified successfully`);
-    }
-    res.send("The game was modified successfully");
+    res.send(await updateGame(bodyGame));
   } catch (error) {
     res.status(404).send(error);
   }
---
+++ api/src/services/game.service.ts
@@ -0,0 +1,82 @@
+import { Game } from "../interfaces/game.interface";
+import GameModel from "../models/game.model";
+
+export const getGames = async (active: string, id?: string | null) => {
+  if (id) {
+    const game = await GameModel.findById(id);
+    //paranoid: false
+    if (!game) throw new Error("The game not found");
+    return game;
+  } else {
+    if (active === "true") {
+      const games = await GameModel.find({
+        where: {
+          active: true,
+        },
+      });
+
+      return games;
+    } else {
+      const allGames = await GameModel.find();
+
+      return allGames;
+    }
+  }
+};
+
+export const updateStatusGame = async (id: string, status: boolean) => {
+  return await GameModel.findByIdAndUpdate(id, {
+    active: status,
+  });
+};
+
+export const deletePermanentlyGame = async (id: string) => {
+  return await GameModel.findByIdAndDelete(id);
+};
+
+export const patchGame = async (
+  id: string,
+  action: string,
+  value: boolean
+) => {
+  const game = await GameModel.findByIdAndUpdate(id, {
+    [action]: value,
+  });
+  if (!game) throw new Error("The game is null");
+  return await GameModel.findById(`${game?._id}`);
+};
+
+export const createGame =async(bodyGame: Game) => {
+    const game = await GameModel.findOne({ name: bodyGame.name });
+    if (game) throw new Error(`This game already exists`);
+    const createdGame = await GameModel.create({
+      name: bodyGame.name,
+      description: bodyGame.description,
+      released: bodyGame.released,
+      main_image: bodyGame.main_image,
+      short_screenshots: bodyGame.short_screenshots,
+      rating: bodyGame.rating,
+      price: Number(bodyGame.price),
+      on_sale: Number(bodyGame.on_sale),
+      stock: Number(bodyGame.stock),
+      featured: bodyGame.featured ? true : false,
+      is_new: bodyGame.is_new ? true : false,
+      platforms: bodyGame.platforms,
+      genres: bodyGame.genres,
+      tags: bodyGame.tags,
+    });
+
+    return `The game was created successfully!`;
+}
+
+export const updateGame = async (bodyGame: any) => {
+    const updatedGame = await GameModel.findByIdAndUpdate(
+        bodyGame._id,
+        bodyGame
+      );
+  
+      if (!updatedGame) {
+        throw new Error(`The game could not be modified successfully`);
+      }
+      return "The game was modified successfully";
+}
