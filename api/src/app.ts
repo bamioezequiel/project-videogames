3543a491a07e3866f1a4c0d610cff1dd35a95de4
Bamio Ezequiel <ezequielbamiok@gmail.com>
2023-02-03 15:46:24 -0300

    cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,22 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import usersRoutes from './routes/users.routes';
-import cartRoutes from './routes/cart.routes';
-import gamesRoutes from './routes/games.routes';
-import paymentRoutes from './routes/payment.routes';
-import categoriesRoutes from './routes/categories.routes';
-import authRoutes from './routes/auth.routes';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import usersRoutes from "./routes/users.routes";
+import cartRoutes from "./routes/cart.routes";
+import gamesRoutes from "./routes/games.routes";
+import paymentRoutes from "./routes/payment.routes";
+import categoriesRoutes from "./routes/categories.routes";
+import authRoutes from "./routes/auth.routes";
 const app = express();
 
-app.use(cors({
-    origin: ['*'],
-    methods: ['POST', 'GET', 'DELETE'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://project-videogames-rosy.vercel.app",
+    ],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use("/auth", authRoutes);
 app.use("/games", gamesRoutes);
 app.use("/cart", cartRoutes);
